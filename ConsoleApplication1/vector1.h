25c62beb2e15430750af776542151d3e89f9497f
thijs hosman <thijs.hosman@gmail.com>
2014-08-01 17:10:58 -0700

    initial commit from git gui

---
+++ ConsoleApplication1/vector1.h
@@ -0,0 +1,12 @@
+
+
+class vector1 {
+public:
+	vector1(int s);
+	double& operator[](int i);
+	int size();
+
+private:
+	double* elem; // pointer to array of sz doubles
+	int sz; // number of elements
+};
